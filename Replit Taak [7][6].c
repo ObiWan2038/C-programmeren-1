498ddd3ef6da95040c59990ac0bea6f74146c45d
ObiWan2038 <114076283+ObiWan2038@users.noreply.github.com>
2022-12-02 15:06:26 +0100

    Create Replit Taak [7][6].c

---
+++ Replit Taak [7][6].c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main( void )
+{
+	// Fix it !
+	int seed;
+	printf( "Geef een geheel getal als seed : \n" );
+	(void)scanf( "%d", &seed );
+	srand( seed );
+	char username[] = "admin";
+	char password[16] = "1234567890abcdef";
+	printf( "Your username = %s\n", username );
+	for( int i = 0; i < 16; i++ )
+	{
+		uint8_t randomSymbol = rand() % ( 26 + 26 + 10 );
+		if( randomSymbol < 26 )
+		{
+			password[i] = 'A' + randomSymbol;
+		}
+		else if( randomSymbol < 26 + 26 )
+		{
+			password[i] = 'a' + randomSymbol - 26;
+		}
+		else
+		{
+			password[i] = '0' + randomSymbol - 26 - 26;
+		}
+	}
+	printf( "Your password = %s\n", password );
+	return 0;
+}
+
+========================================================================================
+  
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main( void )
+{
+    // Fix it !
+    int seed;
+    printf( "Geef een geheel getal als seed : \n" );
+    (void)scanf( "%d", &seed );
+    srand( seed );
+    char username[] = "admin";
+    char password[16] = "";
+    printf( "Your username = %s\n", username );
+    for( int i = 0; i < 15; i++ )
+    {
+        uint8_t randomSymbol = rand() % ( 26 + 26 + 10 );
+        if( randomSymbol < 26 )
+        {
+            password[i] = 'A' + randomSymbol;
+        }
+        else if( randomSymbol < 26 + 26 )
+        {
+            password[i] = 'a' + randomSymbol - 26;
+        }
+        else
+        {
+            password[i] = '0' + randomSymbol - 26 - 26;
+        }
+    }
+    printf( "Your password = %s\n", password );
+    return 0;
+}
