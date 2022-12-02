a924552c0be1cadaae39c48ecc33a2b032e49816
ObiWan2038 <114076283+ObiWan2038@users.noreply.github.com>
2022-12-02 15:01:25 +0100

    Create Replit Taak [7]]0].c

---
+++ Replit Taak [7]]0].c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main( void )
+{
+	// Fix it !
+	char PHL[3] = "PHL";
+	char PXL[3] = "PXL";
+	char Xios[4] = "Xios";
+	printf( "%s + %s = %s", PHL, Xios, PXL );
+	return 0;
+}
+
+
+==================================================================================
+  
+  #include <stdio.h>
+
+int main( void )
+{
+	// Fix it !
+	char PHL[4] = "PHL";
+	char PXL[4] = "PXL";
+	char Xios[5] = "Xios";
+	printf( "%s + %s = %s", PHL, Xios, PXL );
+	return 0;
+}
