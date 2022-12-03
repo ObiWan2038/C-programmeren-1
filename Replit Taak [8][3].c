bc7c325853fe9ac9369d8938fcdd3f9bcf649330
ObiWan2038 <114076283+ObiWan2038@users.noreply.github.com>
2022-12-03 08:19:39 +0100

    Create Replit Taak [8][3].c

---
+++ Replit Taak [8][3].c
@@ -0,0 +1,128 @@
+/*
+  Fix it!
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main( void )
+{
+	unsigned short m = 0;
+
+	do
+	{
+		printf( "Geef het cijfer van een maand: " );
+		(void)scanf( "%hu", &m );
+	} while( m < 0 || m > 13 );
+
+	char maand[10];
+
+	switch( m )
+	{
+	case 1:
+		strcpy( maand, "Januari" );
+	case 2:
+		strcpy( maand, "Februari" );
+	case 3:
+		strcpy( maand, "Maart" );
+	case 4:
+		strcpy( maand, "April" );
+	case 5:
+		strcpy( maand, "Mei" );
+	case 6:
+		strcpy( maand, "Juni" );
+	case 7:
+		strcpy( maand, "Juli" );
+	case 8:
+		strcpy( maand, "Augustus" );
+	case 9:
+		strcpy( maand, "September" );
+	case 10:
+		strcpy( maand, "Oktober" );
+	case 11:
+		strcpy( maand, "November" );
+	case 12:
+		strcpy( maand, "December" );
+	}
+
+	printf( "%s\n", maand );
+
+	return 0;
+}
+
+==================================================================================================
+  
+  /*
+  Fix it!
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main( void )
+{
+	unsigned short m = 0;
+
+	do
+	{
+		printf( "Geef het cijfer van een maand: " );
+		(void)scanf( "%hu", &m );
+	} while( m < 0 || m > 13 );
+
+	char maand[10];
+
+	switch( m )
+	{
+	case 1:
+		strcpy( maand, "Januari" );
+    break;
+    
+	case 2:
+		strcpy( maand, "Februari" );
+    break;
+    
+	case 3:
+		strcpy( maand, "Maart" );
+    break;
+    
+	case 4:
+		strcpy( maand, "April" );
+    break;
+    
+	case 5:
+		strcpy( maand, "Mei" );
+    break;
+    
+	case 6:
+		strcpy( maand, "Juni" );
+    break;
+    
+	case 7:
+		strcpy( maand, "Juli" );
+    break;
+    
+	case 8:
+		strcpy( maand, "Augustus" );
+    break;
+    
+	case 9:
+		strcpy( maand, "September" );
+    break;
+    
+	case 10:
+		strcpy( maand, "Oktober" );
+    break;
+    
+	case 11:
+		strcpy( maand, "November" );
+    break;
+    
+	case 12:
+		strcpy( maand, "December" );
+    break;
+	}
+
+	printf( "%s\n", maand );
+
+	return 0;
+}
