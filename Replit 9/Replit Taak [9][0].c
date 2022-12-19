814cdc5634272330b8ff859eb5b67b076bfdb58f
ObiWan2038 <114076283+ObiWan2038@users.noreply.github.com>
2022-12-19 17:30:19 +0100

    Create Replit Taak [9][0].c

---
+++ Replit 9/Replit Taak [9][0].c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// Create this !
+uint8_t setBitOn( uint8_t data, uint8_t bit )
+{
+}
+
+int main( void )
+{
+	uint8_t getal;
+	uint8_t bit;
+	printf( "Geef een positief geheel getal " );
+	(void)scanf( "%hhu", &getal );
+	printf( "Zeg welke bit aan moet " );
+	(void)scanf( "%hhu", &bit );
+	printf( "Het resultaat is %hhu\n", setBitOn( getal, bit ) );
+	return 0;
+}
+
+=============================================================================================================
+#include <stdio.h>
+#include <stdint.h>
+
+// Create this !
+uint8_t setBitOn( uint8_t getal, uint8_t bit )
+{
+  return getal | (1<<bit);
+}
+
+int main( void )
+{
+	uint8_t getal;
+	uint8_t bit;
+	printf( "Geef een positief geheel getal " );
+	(void)scanf( "%hhu", &getal );
+	printf( "Zeg welke bit aan moet " );
+	(void)scanf( "%hhu", &bit );
+	printf( "Het resultaat is %hhu\n", setBitOn( getal, bit ) );
+	return 0;
+}
+  
