1c41024a2f7d462a7ad9550d2ca38033f4220224
ObiWan2038 <114076283+ObiWan2038@users.noreply.github.com>
2022-12-20 14:25:12 +0100

    Create Replit Taak [9][8].c

---
+++ Replit 9/Replit Taak [9][8].c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdint.h>
+
+char dag[][10] = { "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag", "NoRepeat" };
+
+// Fix it !
+void printDagen( uint8_t data )
+{
+	for( uint8_t i = 0; i < 8; i++ )
+	{
+		if( data && ( 0x1 << i ) )
+		{
+			printf( "%s, ", dag[i] );
+		}
+	}
+}
+
+int main( void )
+{
+	uint8_t alarmSetting;
+	printf( "Geef een hexadecimaal code voor alarm " );
+	(void)scanf( "%hhx", &alarmSetting );
+	printf( "Alarm herhaalt elke " );
+	printDagen( alarmSetting );
+	return 0;
+}
+
+=============================================================================
+  
+#include <stdio.h>
+#include <stdint.h>
+
+char dag[][10] = { "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag", "NoRepeat" };
+
+void printDagen(uint8_t data)
+{
+    for (uint8_t i = 0; i < 7; i++)
+    {
+        if (data & (1 << i))
+        {
+            printf("%s, ", dag[i]);
+        }
+    }
+}
+
+int main( void )
+{
+	uint8_t alarmSetting;
+	printf( "Geef een hexadecimaal code voor alarm " );
+	(void)scanf( "%hhx", &alarmSetting );
+	printf( "Alarm herhaalt elke " );
+	printDagen( alarmSetting );
+	return 0;
+}
